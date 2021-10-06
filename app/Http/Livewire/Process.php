5fd3289a3120da536433f8203d03115ee4c0a5a2
Preterra Corp <victor@preterra.com>
2021-10-06 12:00:27 -0600

    building checklist generator

--- app/Http/Livewire/Process.php
+++ app/Http/Livewire/Process.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\FirmChecklist;
+use App\Models\FirmChecklistItem;
 use Livewire\Component;
 
 use App\Models\Checklist;
@@ -18,9 +20,37 @@ class Process extends Component
 
     public function generateProcess(\App\Models\Process $process){
         error_log($process->name . ' to be processed');
+        foreach($process->checklists as $checklist){
+            // generate for each checklist and then for each item in each checklist
+            $this->generateChecklist($checklist, $process);
+        }
     }
 
-    public function generateChecklist(Checklist $checklist){
+    public function generateChecklist(Checklist $checklist, \App\Models\Process $process = null){
         error_log($checklist->name . ' to be processed');
+
+        $firmChecklist = FirmChecklist::create([
+            'process_id' => $process ? $process->id : null,
+            'process_order' => $process ? $checklist->process_order : 0,
+            'checklist_id' => $checklist->id,
+            'checklistof_id' => 1, //firm
+            'checklistof_type' => "App//Models//Firm",
+            'name' => $checklist->name,
+            'description' => $checklist->description
+        ]);
+
+        foreach($checklist->items as $item){
+            $firmCheckListItem = FirmChecklistItem::create([
+                'firm_checklist_id' => $firmChecklist->id,
+                'firm_parent_item_id' => $item->parent_item_id,
+                'activated_parent_status' => $item->activated_parent_status,
+                'required' => $item->required,
+                'description' => $item->description,
+                'status' => "Pending",
+                'sort_order' => $item->sort_order,
+                'category' => $item->category
+            ]);
+            ;
+        }
     }
 }
