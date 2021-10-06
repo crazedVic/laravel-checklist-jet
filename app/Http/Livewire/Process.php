06153b9a081c65fdc312e087ab459a6f9c17c9d5
Preterra Corp <victor@preterra.com>
2021-10-06 11:24:36 -0600

    building checklist generator

--- app/Http/Livewire/Process.php
+++ app/Http/Livewire/Process.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\Checklist;
+
 class Process extends Component
 {
 
@@ -12,5 +14,13 @@ class Process extends Component
         $processes = \App\Models\Process::all();
         return view('livewire.process',
         ["processes"=> $processes]);
+    }
+
+    public function generateProcess(\App\Models\Process $process){
+        error_log($process->name . ' to be processed');
+    }
+
+    public function generateChecklist(Checklist $checklist){
+        error_log($checklist->name . ' to be processed');
     }
 }
--- resources/views/livewire/process.blade.php
+++ resources/views/livewire/process.blade.php
@@ -1,10 +1,12 @@
 <div>
     <ul>
     @foreach($processes as $process)
-       <li class="font-bold">{{ $process->name }}</li>
+       <li class="font-bold">{{ $process->name }}
+           <button wire:click="generateProcess({{$process}})" class="p-1 m-1 text-white bg-indigo-500">Generate Process</button></li>
         <ol class="ml-5">
         @foreach($process->checklists as $checklist)
-                <li><button wire:click="$emit('poop',{{$checklist}})">{{$checklist->name}}</button></li>
+                <li><button wire:click="$emit('poop',{{$checklist}})">{{$checklist->name}}</button>
+                <button wire:click="generateChecklist({{$checklist}})" class="p-1 m-1 text-white bg-indigo-500">Generate Checklist</button></li>
             @endforeach
         </ol>
     @endforeach
