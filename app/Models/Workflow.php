1c5775cae4feaead9a942253cf915a01e4410250
Preterra Corp <victor@preterra.com>
2022-02-02 10:08:26 -0700

    remove old

--- app/Models/Workflow.php
+++ app/Models/Workflow.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Workflow extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $with = ["firmChecklists","checklists"];
+
+    public function checklists(): HasMany
+    {
+        return $this->hasMany(Checklist::class);
+    }
+
+    public function firmChecklists(): HasMany{
+        return $this->hasMany(FirmChecklist::class);
+    }
 }
