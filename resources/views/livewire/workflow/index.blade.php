c27491a6877a0b7e6993f1d494b5c486a3c12e4c
Preterra Corp <victor@preterra.com>
2022-02-04 11:30:36 -0700

    show dependencies

--- resources/views/livewire/workflow/index.blade.php
+++ resources/views/livewire/workflow/index.blade.php
@@ -9,16 +9,18 @@
                         <button wire:click="$emit('poop',{{$checklist}})">{{$checklist->name}}</button>
                         @foreach($checklist->items as $item)
                             <div class="text-gray-700 ml-7">{{$item->name}}
-                                @if($item->taskDependencies->first())<br>  Depends on Tasks:@endif
-                                @foreach($item->taskDependencies as $taskDependency)
-                                    <div
-                                        class="text-blue-500 ml-5">{{$taskDependency->id}} {{$taskDependency->name}} {{$taskDependency->pivot->required}}</div>
-                                @endforeach
-                                @if($item->checklistDependencies->first())<br>  Depends on Checklists:@endif
-                                @foreach($item->checklistDependencies as $clDependency)
-                                    <div
-                                        class="text-green-500 ml-53">{{$clDependency->id}}  {{$clDependency->name}} {{$clDependency->pivot->required}}</div>
-                                @endforeach
+                                <div class="ml-5">
+                                    @if($item->taskDependencies->first())Depends on Tasks:@endif
+                                    @foreach($item->taskDependencies as $taskDependency)
+                                        <div
+                                            class="text-blue-500 ml-2">{{$taskDependency->id}} {{$taskDependency->name}} with status of {{$taskDependency->pivot->required}}</div>
+                                    @endforeach
+                                    @if($item->checklistDependencies->first())Depends on Checklists:@endif
+                                    @foreach($item->checklistDependencies as $clDependency)
+                                        <div
+                                            class="text-green-500 ml-2">{{$clDependency->id}}  {{$clDependency->name}} with status of {{$clDependency->pivot->required}}</div>
+                                    @endforeach
+                                </div>
                             </div>
                         @endforeach
                     </li>
