dcdd99636dda6317e7d58fec8a899038b0cd5173
Preterra Corp <victor@preterra.com>
2022-02-08 12:59:39 -0700

    added slots

--- resources/views/components/modal-base.blade.php
+++ resources/views/components/modal-base.blade.php
@@ -1,10 +1,17 @@
 @props([
-    'show'=>"showTaskModal"
+    'show'=>"showTaskModal", 'title'=>"Please add Title in \$title slot", 'footer'=>"\$footer slot should have buttons"
 ])
 <div class="fixed inset-0 w-full flex justify-center items-center bg-black bg-opacity-25"
      x-show="{{$show}}">
     <div @click.away="{{$show}} = false;"
-         class="bg-white border border-gray-600 shadow-md rounded p-20">
-        {{$slot}}
+         class="bg-white border border-gray-600 shadow-lg rounded sm:min-h-16 w-full sm:w-3/4 lg:w-1/2">
+        <div class="flex justify-between w-full bg-black text-white px-2 py-1"><span>{{ $title }}</span>
+            <span class="cursor-pointer" @click="{{$show}}=false;">X</span></div>
+        <div class="m-3">
+            {{$slot}}
+        </div>
+        <div class="border-t border-gray-200 w-full flex justify-end space-x-2 px-3 py-2">
+            {{ $footer }}
+        </div>
     </div>
 </div>
--- resources/views/livewire/checklist/modal.blade.php
+++ resources/views/livewire/checklist/modal.blade.php
@@ -1,5 +1,13 @@
 <x-modal-base show="showChecklistModal">
-   Checklist Modal
-    <input type="text">
-    <button wire:click="save()">Save</button>
+    <div>
+        <x-slot name="title">Checklist Modal</x-slot>
+        <div class="w-full px-5">
+            Please enter a name: <input type="text">
+        </div>
+        <x-slot name="footer">
+            <button class="btn bg-gray-800" wire:click="save()">Cancel</button>
+            <button class="btn bg-green-800" wire:click="save()">Save</button>
+            <button class="btn bg-blue-800"  wire:click="save()">Save & Add</button>
+        </x-slot>
+    </div>
 </x-modal-base>
