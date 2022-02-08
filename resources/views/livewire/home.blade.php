11fd9bd30bebeeb60718c9ebea3157b3137c9b7d
Preterra Corp <victor@preterra.com>
2022-02-08 15:19:33 -0700

    styling

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -5,63 +5,72 @@
     {{--        <div class="align-text-bottom text-xl"><i style="font-size: 1.4rem;" class="fas fa-project-diagram text-blue-600 mr-3"></i>HIB Cons. Deal</div>--}}
     {{--        <div class="align-text-bottom text-xl"><i style="font-size: 1.4rem;" class="fas fa-project-diagram text-blue-600 mr-3"></i>Pomagranite Deal</div>--}}
     <div class="align-text-bottom text-xl"><i style="font-size: 1.4rem;"
-                                              class="fas fa-project-diagram text-blue-600 mr-3"></i>
+                                              class="fas fa-project-diagram text-red-600 mr-3"></i>
         AppDev Ltd Deal
     </div>
     <div class="text-xl pl-6"><i style="font-size: 1.4rem;" class="fas fa-list text-red-600 mr-3"></i>
-        LOI Due Diligence
-        <i style="font-size: 1rem;" class="fas fa-hourglass text-red-500 mr-3"></i>
+        Due Diligence
+        <i style="font-size: 1rem;" class="fas fa-hourglass text-red-600 mr-3"></i>
     </div>
     <div class="text-xl pl-12"><i style="font-size: 1.4rem;" class="fas fa-tasks text-red-600 mr-3"></i>
         Organization
-        <i style="font-size: 1rem;" class="fas fa-hourglass text-green-500"></i>
-        <span class="text-green-500 text-lg mr-2">23%</span>
-        <i style="font-size: 1rem;" class="fas fa-hourglass text-blue-500"></i>
-        <span class="text-blue-500 text-lg">7%</span>
+        <i style="font-size: 1rem;" class="fas fa-hourglass text-green-600"></i>
+        <span class="text-green-600 text-lg mr-2">23%</span>
+        <i style="font-size: 1rem;" class="fas fa-hourglass text-blue-600"></i>
+        <span class="text-blue-600 text-lg">7%</span>
     </div>
     <div class="ml-24">
-        <p class="text-xl pl-20 text-blue-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                     class="fas fa-check text-blue-600 mr-3"></i>
+        <p class="text-xl pl-20 text-blue-600 -indent-8"><i style="font-size: 1.2rem;"
+                                                            class="fas fa-check text-blue-600 mr-2"></i>
             Articles of Incorporation
         </p>
-        <p class="text-xl pl-20 text-blue-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                     class="fas fa-check text-blue-600 mr-3"></i>
+        <p class="text-xl pl-20 text-blue-600 -indent-8"><i style="font-size: 1.2rem;"
+                                                            class="fas fa-check text-blue-600 mr-2"></i>
             Company Bylaws
-                  </p>
-        <p class="text-xl pl-20 text-green-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                     class="fas fa-check text-green-600 mr-3"></i>
-            Organization Charts
-
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 text-green-600 -indent-8"><i style="font-size: 1.2rem;"
+                                                             class="fas fa-check text-green-600 mr-2"></i>
+            Organization Charts
+        </p>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Capital Structure
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Equity Positions
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Option Holders
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Warrant Holders
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Voting Trusts
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Other financial agreements
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             Certificate of Good Standing from Province in which company is incorporated
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             List of regions in which the company is authorized to do business
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             List of regions in which the company has employees, contract relationships, leases facilities, or transacts
             business
         </p>
-        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;" class="fas fa-file-upload text-red-600 mr-3"></i>
+        <p class="text-xl pl-20 -indent-8"><i style="font-size: 1.4rem;"
+                                              class="fas fa-file-upload text-red-600 mr-3"></i>
             List of any names (and documentation) under which the company may operate
         </p>
     </div>
@@ -71,17 +80,17 @@
         <span class="text-green-600 text-lg">100%</span>
     </div>
     <p class="text-xl pl-20 text-green-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                 class="fas fa-file-upload text-green-600 mr-3"></i>
+                                                         class="fas fa-file-upload text-green-600 mr-3"></i>
         Equipment leases
         <i style="font-size: 1rem;" class="fas fa-check text-green-600 mr-3"></i>
     </p>
     <p class="text-xl pl-20 text-green-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                 class="fas fa-file-upload text-green-600 mr-3"></i>
+                                                         class="fas fa-file-upload text-green-600 mr-3"></i>
         Company/Auditor correspondence from previous three years
         <i style="font-size: 1rem;" class="fas fa-check text-green-600 mr-3"></i>
     </p>
     <p class="text-xl pl-20 text-green-600 -indent-8"><i style="font-size: 1.4rem;"
-                                                 class="fas fa-file-upload text-green-600 mr-3"></i>
+                                                         class="fas fa-file-upload text-green-600 mr-3"></i>
         Transactions relating to major capital equipment in previous three years
         <i style="font-size: 1rem;" class="fas fa-check text-green-600 mr-3"></i>
     </p>
